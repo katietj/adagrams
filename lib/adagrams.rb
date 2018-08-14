1fc0969d7d5caddeaa6e07a5fbf9c065ea487d06
KatherineJF <kjfitzpat@hotmail.com>
2018-08-13 17:03:07 -0700

    Changed data structure to array

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,18 +1,7 @@
 #Katie and Katherine Anagrams
 
-users_letters =[]
-
-letters = {"A"=>9, "B"=>2, "C"=>2,"D" =>4, "E"=>12, "F"=>2, "G"=>3, "H"=>2, "I"=>9, "J"=>1, "K"=>1, "L"=>4, "M"=>2, "N"=>6, "O"=>8, "P"=>2, "Q"=>1, "R"=>6,
-"S"=>4, "T"=>6, "U" =>4, "V"=>2, "W"=>2, "X"=>1, "Y"=>2, "Z"=>1}
-
+letters = %w[a,a,a,a,a,a,a,a,a,a,b,b,c,c,d,d,d,d,e,e,e,e,e,e,e,e,e,e,e,e,f,f,g,g,g,h,h,i,i,i,i,i,i,i,i,i,k,l,l,l,l,m,m,n,n,n,n,n,n,o,o,o,o,o,o,o,o,p,p,q,r,r,r,r,r,r,s,s,s,s,t,t,t,t,t,t,u,u,u,u,v,v,w,w,x,y,y,z]
+user_hand = []
 def draw_letters
-weighted(weighted)
-  sum = letters.inject(0) do |sum, item_and_weight|
-    sum += item_and_weight[1]
-  end
-  target = rand(sum)
-    letters.each do |item, weight|
-    return item if target <= weight
-    target -= weight
-  end
-end
+  10.times do
+  user_hand << letters.delete_at(rand(letters.length))
