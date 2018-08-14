b4bc217af55a482c01935c0470919ee2f58baa3b
KatherineJF <kjfitzpat@hotmail.com>
2018-08-14 15:25:21 -0700

    green wave 1

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -1,7 +1,18 @@
-#Katie and Katherine Anagrams
+#Katie and Katherine
 
-letters = %w[a,a,a,a,a,a,a,a,a,a,b,b,c,c,d,d,d,d,e,e,e,e,e,e,e,e,e,e,e,e,f,f,g,g,g,h,h,i,i,i,i,i,i,i,i,i,k,l,l,l,l,m,m,n,n,n,n,n,n,o,o,o,o,o,o,o,o,p,p,q,r,r,r,r,r,r,s,s,s,s,t,t,t,t,t,t,u,u,u,u,v,v,w,w,x,y,y,z]
-user_hand = []
+letters = %w(a a a a a a a a a b b c c d d d d e e e e e e e e e e e e f f g g g h h i i i i i i i i i k l l l l m m n n n n n n o o o o o o o o p p q r r r r r r s s s s t t t t t u u u u v v w w x y y z)
+users_hand = []
+
 def draw_letters
-  10.times do
-  user_hand << letters.delete_at(rand(letters.length))
+letters = %w(a a a a a a a a a b b c c d d d d e e e e e e e e e e e e f f g g g h h i i i i i i i i i k l l l l m m n n n n n n o o o o o o o o p p q r r r r r r s s s s t t t t t u u u u v v w w x y y z)
+users_hand = []
+  letter_pool = letters.map do |letters|
+    letters
+  end
+    10.times do |letters|
+      users_hand << letter_pool.delete_at(rand(letter_pool.length))
+    end
+ return users_hand
+end
+
+puts "These are the letters you can use: #{draw_letters}"
