cf4f63379014da50726827acb3ac031c2f5cd1c9
KatherineJF <kjfitzpat@hotmail.com>
2018-08-15 16:20:34 -0700

    Wave 3

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -38,3 +38,83 @@ def uses_available_letters?(input, letters_in_hand)
 end
 
 uses_available_letters?(input, letters_in_hand)
+
+def score_word(word)
+  word = word.split('').each {|letter|}
+  score = 0
+  if word.length >= 7
+    score += 8
+  end
+  word.each do |letter|
+    case letters
+    when "a"
+      score += 1
+end
+
+def score_word(word)
+  word = word.split('').each {|letter|}
+  score = 0
+  word.each do |letter|
+    case letter
+    when "a"
+      score += 1
+    when "b"
+      score += 3
+    when "c"
+      score += 3
+    when "d"
+      score += 2
+    when "e"
+      score += 1
+    when "f"
+      score += 3
+    when "g"
+      score += 2
+    when "h"
+      score += 4
+    when "i"
+      score += 1
+    when "j"
+      score += 8
+    when "k"
+      score += 5
+    when "l"
+      score += 1
+    when "m"
+      score += 3
+    when "n"
+      score += 1
+    when "o"
+      score += 1
+    when "p"
+      score += 3
+    when "q"
+      score += 10
+    when "r"
+      score += 1
+    when "s"
+      score += 1
+    when "t"
+      score += 1
+    when "u"
+      score += 1
+    when "v"
+      score += 4
+    when "w"
+      score += 4
+    when "x"
+      score += 8
+    when "y"
+      score += 4
+    when "z"
+      score += 10
+    end
+  end
+    if word.length >= 7
+      score += 8
+  end
+return score
+end
+
+
+puts score_word("cat")
