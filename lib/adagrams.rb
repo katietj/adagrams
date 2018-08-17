fdca122bb993b18da6a07ba419d6505fb5e56106
KatherineJF <kjfitzpat@hotmail.com>
2018-08-17 15:06:18 -0700

    Wave 4 before test

--- lib/adagrams.rb
+++ lib/adagrams.rb
@@ -40,81 +40,115 @@ uses_available_letters?(input, letters_in_hand)
 
 
 def score_word(word)
- word = word.split('').each {|letter|}
- score = 0
- word.each do |letter|
-   case letter
-   when "a","A"
-     score += 1
-   when "b", "B"
-     score += 3
-   when "c", "C"
-     score += 3
-   when "d", "D"
-     score += 2
-   when "e", "E"
-     score += 1
-   when "f", "F"
-     score += 3
-   when "g", "G"
-     score += 2
-   when "h", "H"
-     score += 4
-   when "i", "I"
-     score += 1
-   when "j", "J"
-     score += 8
-   when "k", "K"
-     score += 5
-   when "l", "L"
-     score += 1
-   when "m", "M"
-     score += 3
-   when "n", "N"
-     score += 1
-   when "o", "O"
-     score += 1
-   when "p", "P"
-     score += 3
-   when "q", "Q"
-     score += 10
-   when "r", "R"
-     score += 1
-   when "s", "S"
-     score += 1
-   when "t", "T"
-     score += 1
-   when "u", "U"
-     score += 1
-   when "v", "V"
-     score += 4
-   when "w", "W"
-     score += 4
-   when "x", "X"
-     score += 8
-   when "y", "Y"
-     score += 4
-   when "z," "Z"
-     score += 10
-   end
- end
-   if word.length >= 7
-     score += 8
- end
-return score
+  word = word.split('').each {|letter|}
+  score = 0
+  word.each do |letter|
+    case letter
+    when "a","A"
+      score += 1
+    when "b", "B"
+      score += 3
+    when "c", "C"
+      score += 3
+    when "d", "D"
+      score += 2
+    when "e", "E"
+      score += 1
+    when "f", "F"
+      score += 3
+    when "g", "G"
+      score += 2
+    when "h", "H"
+      score += 4
+    when "i", "I"
+      score += 1
+    when "j", "J"
+      score += 8
+    when "k", "K"
+      score += 5
+    when "l", "L"
+      score += 1
+    when "m", "M"
+      score += 3
+    when "n", "N"
+      score += 1
+    when "o", "O"
+      score += 1
+    when "p", "P"
+      score += 3
+    when "q", "Q"
+      score += 10
+    when "r", "R"
+      score += 1
+    when "s", "S"
+      score += 1
+    when "t", "T"
+      score += 1
+    when "u", "U"
+      score += 1
+    when "v", "V"
+      score += 4
+    when "w", "W"
+      score += 4
+    when "x", "X"
+      score += 8
+    when "y", "Y"
+      score += 4
+    when "z," "Z"
+      score += 10
+    end
+  end
+  if word.length >= 7
+    score += 8
+  end
+  return score
 end
 
-def highest_score_from_words (words)
- scores = []
- word_scores = []
- words.each do |word|
-   scores << score_word(word)
- end
- i = 0
- words.each do |key, value|
-   word_scores << {word:words[i], score:scores[i]}
-   i += 1
- end
+def highest_score_from_words(words)
+  scores = []
+  lengths = []
+  word_scores = []
+  winner_array = []
+  highest_score = 0
+  words.each do |word|
+    scores << score_word(word)
+  end
 
- return word_scores
+  words.each do |word|
+    lengths << word.length
+  end
+
+
+  i = 0
+  words.each do |key, value|
+    word_scores << {word:words[i], score:scores[i]}
+    i += 1
+  end
+
+  final_winner = []
+  min_lengths = 0
+  lowest_word_length = 0
+  highest_score = scores.max
+
+  word_scores.each do |word_hash|
+    if word_hash[:score] == highest_score
+      winner_array << word_hash
+    end
+  end
+
+  if winner_array.length < 1
+    winner_array.each do |word_hash|
+      if word_hash[:word].length == 10
+        final_winner << word_hash
+      end
+    end
+
+    final_winner.sort_by {|x| x[:word].length}
+    return final_winner.first
+  else
+    return winner_array.first
+  end
 end
+
+
+p
